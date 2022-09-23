c11162bf86e095f3e50e9e15b6d7161f732cd557
Dima <akmeev2011@yandex.ru>
2022-09-23 19:15:48 +0300

    Дз1 задание 3

--- src/Car.java
+++ src/Car.java
@@ -6,19 +6,17 @@ public class Car {
     Integer productionYear;
     String productionCountry;
 
-    Car() {
 
+
+    Car(String brand, String model, double engineVolume, String colour,
+        Integer productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.colour = colour;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
     }
-
-//    Car(String brand, String model, double engineVolume, String colour,
-//        Integer productionYear, String productionCountry) {
-//        this.brand = brand;
-//        this.model = model;
-//        this.engineVolume = engineVolume;
-//        this.colour = colour;
-//        this.productionYear = productionYear;
-//        this.productionCountry = productionCountry;
-//    }
 
     @Override
     public String toString() {
@@ -28,9 +26,4 @@ public class Car {
     }
 
 
-//    public void printCars() {
-//        System.out.println("Марка - " + brand + ". Модель - " + model + ". Объем двигателя - " + engineVolume +
-//                ". Цвет - " + colour + ". Год выпуска - " + productionYear + ". Страна производства - " +
-//                productionCountry);
-//    }
 }
